1c9b948d69c202f398ff3858d6b94a344adb8706
Butter Shara <bshara@vo-i1.21-school.ru>
2019-05-01 16:22:41 +0300

    some fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bshara <bshara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 12:08:58 by chubaka358        #+#    #+#             */
-/*   Updated: 2019/04/28 19:38:23 by bshara           ###   ########.fr       */
+/*   Updated: 2019/05/01 16:04:47 by bshara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ int			get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !line || read(fd, s, 0) < 0)
 		return (-1);
-	while (len = read(fd, s, BUFF_SIZE))
+	while ((len = read(fd, s, BUFF_SIZE)))
 	{
-		if (is_eof != -1)
+		if (is_eof(s) != -1)
 			break ;
-		*line = ft_strjoin(line, )
+		*line = ft_strjoin(*line, ft_cpyuntil(s, EOF));
+		return (1);
 	}
-	
-	return (1);
+	return (0);
 }
