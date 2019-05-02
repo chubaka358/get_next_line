4e79d6052aacbdf20bdc645cd82815de4ffbc021
Butter Shara <bshara@vo-h2.21-school.ru>
2019-05-02 18:54:32 +0300

    work work work

--- get_next_line.c
+++ get_next_line.c
@@ -6,29 +6,45 @@
 /*   By: bshara <bshara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 12:08:58 by chubaka358        #+#    #+#             */
-/*   Updated: 2019/05/02 15:51:32 by bshara           ###   ########.fr       */
+/*   Updated: 2019/05/02 18:48:30 by bshara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int			ft_new_line(char **line, const int fd, char **s)
 {
-	
+	int		len;
+	char	*tmp;
+
+	len = 0;
+	while (s[fd][len] != '\n' && s[fd][len] != '\0')
+	{
+		len++;
+	}
+	if (len == 0)
+		return (0);
+	*line = ft_strsub(s[fd], 0, len);
+	tmp = ft_strsub(s[fd], len, ft_strlen(s[fd]));
+	ft_strdel(&s[fd]);
+	s[fd] = tmp;
+	return (1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-    char	buf[BUFF_SIZE + 1];
-	int		len;
-	char	*s[255];
-	char	*tmp;
+	printf("here");
+    char		buf[BUFF_SIZE + 1];
+	int			ret;
+	static char	*s[255];
+	char		*tmp;
 
 	if (fd < 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
-	while ((len = read(fd, buf, BUFF_SIZE)))
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
-		buf[len] = '\0';
+		buf[ret] = '\0';
 		if (s[fd] == NULL)
 			s[fd] = ft_strnew(1);
 		tmp = ft_strjoin(s[fd], buf);
@@ -37,7 +53,9 @@ int			get_next_line(const int fd, char **line)
 		if (strchr(s[fd], '\n'))
 			break;
 	}
-	if (len == -1)
+	if (ret == -1)
 		return (-1);
-
+	if ((ret == 0) && (buf[ret] == '\0'))
+		return (0);
+	return (ft_new_line(line, fd, s));
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bshara <bshara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 17:22:57 by bshara            #+#    #+#             */
-/*   Updated: 2019/05/02 15:34:36 by bshara           ###   ########.fr       */
+/*   Updated: 2019/05/02 18:23:33 by bshara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@
 int		main()
 {
 	int fd;
-	char *s;
-	
+	char **s;
+	s = malloc(sizeof(char*) * 1);
+	*s = malloc(BUFF_SIZE);
 	fd = open("42.txt", O_RDONLY);
-	while (get_next_line(fd, &s) > 0)
+	while (get_next_line(fd, s) > 0)
 	{
-		printf("%s\n", s);
-		free(s);
+		printf("pi`d\n");
+		printf("%s\n", *s);
+		free(*s);
 	}
 	return (0);
 }
